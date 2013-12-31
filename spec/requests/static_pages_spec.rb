e5f07dda09b10d37a2eac7c75cc638f505a86b75
Ronney Martin <ronney@lesmartin.ca>
2013-12-31 00:05:29 -0500

    tests fonctionnels

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,41 +1,107 @@
 require 'spec_helper'
 
 describe "Pages statiques" do
-	describe "La page d'accueil" do
+
+	let(:base_title) { "Simple App du Tutoriel Ruby on Rails" }
+
+	describe "La page Accueil" do
+
 		it "contient l'expression 'Accueil'" do
 			visit '/'
 			expect(page).to have_content "Accueil"
 		end
-	end
-	it "devrait trouver une page Contact à '/contact'" do
-		visit '/contact'
-		expect(find 'title').to have_content "Contact"
+
+		it "a pour titre 'Accueil'" do
+			visit '/'
+			expect(page).to have_title "#{base_title} | Accueil"
+		end
 	end
 
-	it "devrait trouver une page À Propos à '/about'" do
-		visit '/about'
-		expect(find 'title').to have_content "À Propos"
+	describe "La page Contact" do
+
+		it "contient l'expression 'Contact'" do
+			visit '/contact'
+			expect(page).to have_content "Contact"
+		end
+
+		it "a pour titre 'Contact'" do
+			visit '/contact'
+			expect(page).to have_title "#{base_title} | Contact"
+		end
 	end
 
-	it "devrait trouver une page Aide à '/help'" do
-		visit '/help'
-		expect(find 'title').to have_content "Aide"
+	describe "La page Aide" do
+
+		it "contient l'expression 'Aide'" do
+			visit '/help'
+			expect(page).to have_content "Aide"
+		end
+
+		it "a pour titre 'Aide'" do
+			visit '/help'
+			expect(page).to have_title "#{base_title} | Aide"
+		end
 	end
-	it "devrait avoir une page d'inscription à'/signup'" do
-		visit '/signup'
-		expect(find 'title').to have_content "Inscription"
+
+	describe "La page À Propos" do
+
+		it "contient l'expression 'À Propos'" do
+			visit '/about'
+				visit root_path
+			expect(page).to have_content "À Propos"
+		end
+
+		it "a pour titre 'À Propos'" do
+			visit '/about'
+			expect(page).to have_title "#{base_title} | À Propos"
+		end
 	end
-	it "devrait avoir le bon lien sur le layout" do
-		visit root_path
-		click_link "À Propo"
-		expect(find 'title').to have_content "À Propos"
-		click_link "Aide"
-		expect(find 'title').to have_content "Aide"
-		click_link "Contact"
-		expect(find 'title').to have_content "Contact"
-		click_link "Accueil"
-		expect(find 'title').to have_content "Accueil"
-		click_link "S'inscrire"
-		expect(find 'title').to have_content "Inscription"
+
+	describe "Les hyperliens statiques sur les en-têtes et pieds de page" do
+
+		describe "Le lien 'À Propos" do
+
+			it "mène à la page 'À Propos'" do
+				visit root_path
+				click_link "À Propos"
+				expect(find 'title').to have_content "À Propos"
+			end
+		end
+
+		describe "Le lien 'Aide'" do
+
+			it "mène à la page 'Aide'" do
+				visit root_path
+				click_link "Aide"
+				expect(find 'title').to have_content "Aide"
+			end
+		end
+
+		describe "Le lien 'Contact'" do
+
+			it "mène à la page 'Contact'" do
+				visit root_path
+				click_link "Contact"
+				expect(find 'title').to have_content "Contact"
+			end
+		end
+
+		describe "Le lien 'Accueil'" do
+
+			it "mène à la page 'Accueil'" do
+				visit root_path
+				click_link "Accueil"
+				expect(find 'title').to have_content "Accueil"
+			end 
+		end
+
+		describe "Le lien 'Inscription'" do
+
+			it "mène à la page 'Inscription'" do
+				visit root_path
+				click_link "S'inscrire"
+				expect(find 'title').to have_content "Inscription"
+			end
+		end
 	end
 end
